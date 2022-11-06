fbfd7bc83bd7654d6922219d68251a210bc4f858
Maksim Muhomedyarov <madmaxim22@gmail.com>
2022-11-06 17:35:59 +0500

    Add "CSV - JSON parser"

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,77 @@
+package org.example;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    public long id;
+    @SerializedName("name")
+    public String firstName;
+    @SerializedName("surname")
+    public String lastName;
+    public String country;
+    public int age;
+
+    public Employee() {
+    }
+
+    public Employee(long id, String firstName, String lastName, String country, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
